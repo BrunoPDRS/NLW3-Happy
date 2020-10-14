741bddd34387a7e77fd0520ef0ba397b44296886
Bruno Pedrosa <bgpedrosas@gmail.com>
2020-10-14 12:51:27 -0300

    Finished form layout and separated css components

--- frontend/js/page-orphanage.js
+++ frontend/js/page-orphanage.js
@@ -4,25 +4,24 @@ const options = {
   doubleClickZoom: false,
   scrollWheelZoom: false,
   zoomControl: false,
-}
+};
 
 // creating a map
-var map = L.map('mapid', options).setView([-23.0019223,-44.3011119], 15.25);
+var map = L.map("mapid", options).setView([-23.0019223, -44.3011119], 15.25);
 
 // creating a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // creating an icon
 const icon = L.icon({
   iconUrl: "../images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
+  popupAnchor: [170, 2],
 });
 
 // creating and adding marker
-L.marker([-23.0019223,-44.3011119], { icon }).addTo(map);
-
+L.marker([-23.0019223, -44.3011119], { icon }).addTo(map);
 
 /* image gallery */
 
@@ -31,7 +30,7 @@ function selectImage(event) {
 
   // removendo classes active
   const buttons = document.querySelectorAll(".images button");
-  buttons.forEach(button => button.classList.remove("active"));
+  buttons.forEach((button) => button.classList.remove("active"));
 
   // selecionando a imagem clicada
   const image = button.children[0];
--- frontend/js/page-orphanages.js
+++ frontend/js/page-orphanages.js
@@ -1,25 +1,26 @@
 // creating a map
-var map = L.map('mapid').setView([-23.0019223,-44.3011119], 15.25);
+var map = L.map("mapid").setView([-23.0019223, -44.3011119], 15.25);
 
 // creating a tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
 // creating an icon
 const icon = L.icon({
   iconUrl: "../images/map-marker.svg",
   iconSize: [58, 68],
   iconAnchor: [29, 68],
-  popupAnchor: [170, 2]
+  popupAnchor: [170, 2],
 });
 
 // creating popup overlay
 const popup = L.popup({
   closeButton: false,
-  className: 'map-popup',
+  className: "map-popup",
   minWidth: 240,
-  minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="../images/arrow-white.svg" /> </a>')
+  minHeight: 240,
+}).setContent(
+  'Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="../images/arrow-white.svg" /> </a>'
+);
 
 // creating and adding marker
-L.marker([-23.0019223,-44.3011119], { icon }).addTo(map)
-    .bindPopup(popup);
+L.marker([-23.0019223, -44.3011119], { icon }).addTo(map).bindPopup(popup);
